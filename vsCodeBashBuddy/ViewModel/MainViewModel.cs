1c65d33460534f2ff902ff3f63ba37eeae8eebb3
muqluk@yahoo.com <muqluk@yahoo.com>
2019-01-21 09:18:21 -0600

    added a new plugin

--- vsCodeBashBuddy/ViewModel/MainViewModel.cs
+++ vsCodeBashBuddy/ViewModel/MainViewModel.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Controls;
+
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 
@@ -5,18 +12,39 @@ using GalaSoft.MvvmLight.Command;
 namespace vsCodeBashBuddy.ViewModel {
   public class MainViewModel : ViewModelBase {
 
+    public ObservableCollection<UserControl> Controls { get; set; }
+
     public RelayCommand HandleWindowClosingCmd {
       get { return new RelayCommand(HandleWindowClosing, () => true); }
     }
 
-    public MainViewModel() { }
+    public MainViewModel() {
+      //var viewModels = (App.Current.Resources["Locator"] as ViewModel.ViewModelLocator).ViewModels.FindAll(c => c.Name != "Main");
+      //Controls = new ObservableCollection<UserControl>();
+      //Assembly.GetExecutingAssembly()
+      //  .GetTypes()
+      //  //.Where(t => {
+      //  //  return t.GetInterfaces().Contains(typeof(UserControl));
+      //  //})
+      //  .ToList()
+      //  .ForEach(view => {
+      //    if (view.IsSubclassOf(typeof(UserControl))) {
+      //      //Controls.Add(view. as UserControl);
+      //      var a = 1;
+      //      var instance = Activator.CreateInstance(view) as UserControl;
+      //      Controls.Add(instance);
+      //    }
+      //    //Controls.Add(instance);
+      //  });
+      var a = 1;
+    }
 
 
     #region Application Event Handlers
 
     // perform all application thread clean up.
     private void HandleWindowClosing() {
-      int attempts = 0;
+      //int attempts = 0;
     }
 
     #endregion
