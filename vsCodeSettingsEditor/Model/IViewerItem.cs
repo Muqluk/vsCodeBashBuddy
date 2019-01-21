7b445b0feefe019b6f84a8ccc3f22322094327e4
muqluk@yahoo.com <muqluk@yahoo.com>
2019-01-21 09:18:38 -0600

    added a new plugin part 2

---
+++ vsCodeSettingsEditor/Model/IViewerItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vsCodeSettingsEditor.Model {
+  interface IViewerItem {
+    string Name { get; set; }
+    string Path { get; set; }
+
+  }
+}
