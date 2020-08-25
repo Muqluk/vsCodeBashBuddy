f0cb64efa544a89533c681c5a3c9016b02cdfe8f
Jeremy Little <jwlittle@maxor.com>
2020-08-25 11:23:38 -0500

    not working but should

--- vsCodeBashBuddy/Model/FileFolderItem.cs
+++ vsCodeBashBuddy/Model/FileFolderItem.cs
@@ -1,35 +1,61 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using GalaSoft.MvvmLight;
 
 namespace vsCodeBashBuddy.Model {
 
   public enum FileFolderType {
     Directory,
     File,
+    Root,
   }
 
   public interface IFileFolderItem {
-    FileFolderType Type { get; set; }
-    string Path { get; set; }
     string Name { get; set; }
-    int ChildCount { get; set; }
-    int Size { get; set; }
-    IEnumerable<IFileFolderItem> Descendents { get; set; }
+    string Path { get; set; }
 
-    int TotalChildCount { get; set; }
-    int TotalSize { get; set; }
-    IEnumerable<IFileFolderItem> TotalDescendents { get; set; }
+    bool isLoaded { get; set; }
+    FileFolderType FileFolderType { get; set; }
+
+    ObservableCollection<IFileFolderItem> Descendents { get; set; }
+    //int ChildCount { get; set; }
+    long Size { get; set; }
+    string Extension { get; set; }
+
+    //int TotalChildCount { get; set; }
+    //int TotalSize { get; set; }
+    //ObservableCollection<IFileFolderItem> TotalDescendents { get; set; }
   }
 
-  public class FileFolderItem : IFileFolderItem {
-    public FileFolderType Type { get; set; }
+  public class FileFolderItem : ViewModelBase, IFileFolderItem {
+    private string _name = "";
+    public string Name {
+      get {
+        return _name;
+      }
+      set {
+        if (value != _name) {
+          _name = value;
+          RaisePropertyChanged(nameof(Name));
+        }
+      }
+    }
     public string Path { get; set; }
-    public string Name { get; set; }
-    public int ChildCount { get; set; }
-    public int Size { get; set; }
-    public IEnumerable<IFileFolderItem> Descendents { get; set; }
-    public int TotalChildCount { get; set; }
-    public int TotalSize { get; set; }
-    public IEnumerable<IFileFolderItem> TotalDescendents { get; set; }
+    public bool isLoaded { get; set; }
+    public FileFolderType FileFolderType { get; set; }
+
+    public ObservableCollection<IFileFolderItem> Descendents {
+      get;
+      set;
+    }
+    public long Size { get; set; }
+    public string Extension { get; set; }
   }
+  //public int ChildCount { get; set; }
+  //public int TotalChildCount { get; set; }
+  //public int TotalSize { get; set; }
+  //public ObservableCollection<IFileFolderItem> TotalDescendents { get; set; }
 
 }
--- vsCodeBashBuddy/ViewModel/vsCodeSettingsViewModel.cs
+++ vsCodeBashBuddy/ViewModel/vsCodeSettingsViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Windows.Navigation;
 
 using GalaSoft.MvvmLight;
 
@@ -16,10 +17,89 @@ namespace vsCodeBashBuddy.ViewModel {
     public override void DisposeThreads() { }
     #endregion
 
+    #region Private Members
+
     // add a directory browse dialog and button later.  For now, hard code.
     const string startupPath = @"C:\repo\gateway\ProOpt\ui-prooptimizer";
 
-    ObservableCollection<IFileFolderItem> Repository { get; set; }
+    #endregion
+
+    #region Properties
+
+    private IFileFolderItem _activeRepo = new FileFolderItem();
+    protected IFileFolderItem ActiveRepository {
+      get {
+        return _activeRepo;
+      }
+      set {
+        if (value != _activeRepo) {
+          _activeRepo = value;
+          RepoItems = value.Descendents.ToList();
+          RaisePropertyChanged(nameof(ActiveRepository));
+        }
+      }
+    }
+
+    private List<IFileFolderItem> _repoItems;
+    protected List<IFileFolderItem> RepoItems {
+      get {
+        return _repoItems;
+      }
+      set {
+        if (value != _repoItems) {
+          _repoItems = value;
+          RaisePropertyChanged(nameof(RepoItems));
+        }
+      }
+    }
+
+    #endregion
+
+    #region CTOR
+
+    public vsCodeSettingsViewModel() {
+      ActiveRepository = LoadDirectory(startupPath);
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private IFileFolderItem LoadDirectory(string fromPath) {
+      IFileFolderItem ffItem = new FileFolderItem() {
+        Name = Path.GetDirectoryName(fromPath),
+        Path = fromPath,
+        isLoaded = true,
+        FileFolderType = FileFolderType.Root,
+        Descendents = new ObservableCollection<IFileFolderItem>(),
+      };
+
+      foreach (var dir in Directory.GetDirectories(fromPath)) {
+        var item = new FileFolderItem() {
+          Name = Path.GetDirectoryName(dir),
+          Path = dir,
+          isLoaded = false,
+          FileFolderType = FileFolderType.Directory,
+          Descendents = new ObservableCollection<IFileFolderItem>(),
+        };
+        ffItem.Descendents.Add(item);
+      }
+      foreach (var file in Directory.GetFiles(fromPath)) {
+        var fi = new FileInfo(file);
+        var item = new FileFolderItem() {
+          Name = fi.Name,
+          Path = fi.FullName,
+          FileFolderType = FileFolderType.File,
+          Size = fi.Length,
+          Extension = fi.Extension,
+        };
+        ffItem.Descendents.Add(item);
+      }
+
+      return ffItem;
+    }
+
+    #endregion
 
   }
 }
